46ec53d5ad3c1b8ab81ea71e20f3513de89d5e28
xjp <xjpsjtu@sjtu.edu.cn>
2016-10-23 00:50:45 +0800

    ...

--- KNN.py
+++ KNN.py
@@ -19,3 +19,35 @@ def classify0(inX, dataSet, labels, k):
 		classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
 	sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
 	return sortedClassCount[0][0]
+
+def file2matrix(filename):
+	fr = open(filename);
+	arrayOLines = fr.readlines()
+	numberOfLines = len(arrayOLines)
+	returnMat = zeros((numberOfLines, 3))
+	classLabelVector = []
+	labels = {"largeDoses":3, "smallDoses":2, "didntLike":1}
+	index = 0
+	for line in arrayOLines:
+		line = line.strip()
+		listFromLine = line.split('\t')
+		# print(listFromLine)
+		# returnMat[index,:] = listFromLine[0:3]
+		lindex = 0
+		for listline in listFromLine[0:3]:
+			# print(listline)
+			returnMat[index, lindex] = float(listline)
+			lindex += 1
+		classLabelVector.append(labels[listFromLine[-1]])
+		index += 1
+	return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+	minVals = dataSet.min(0)
+	maxVals = dataSet.max(0)
+	ranges = maxVals - minVals
+	normDataSet = zeros(shape(dataSet))
+	m = dataSet.shape[0]
+	normDataSet = dataSet - tile(minVals, (m, 1))
+	normDataSet = normDataSet / tile(ranges, (m,1))
+	return normDataSet, ranges, minVals
