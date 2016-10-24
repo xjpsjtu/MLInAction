224d814dcee09ba3d4f8d5d419a894df6a11bff7
xjp <xjpsjtu@sjtu.edu.cn>
2016-10-24 23:49:07 +0800

    ...

--- KNN.py
+++ KNN.py
@@ -1,5 +1,7 @@
 from numpy import *
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
 
 def createDataSet():
 	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -51,3 +53,37 @@ def autoNorm(dataSet):
 	normDataSet = dataSet - tile(minVals, (m, 1))
 	normDataSet = normDataSet / tile(ranges, (m,1))
 	return normDataSet, ranges, minVals
+
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+		print("the classfier came back with: %d, the real answer is: %d" %(classifierResult, datingLabels[i]))
+		if(classifierResult != datingLabels[i]):
+			errorCount += 1.0
+		print("the total error rate is: %f" %(errorCount/float(numTestVecs)))
+
+def classifyPerson():
+	resultList = ['not at all', 'in small doses', 'in large doses']
+	percentTats = float(input("percentage of time spent playing video games?"))
+	ffMiles = float(input("frequent flier miles earned per year?"))
+	iceCream = float(input("liters of ice cream consumed per year?"))
+	datingDataMat, datingLabels = file2matrix("datingTestSet.txt")
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	inArr = array([ffMiles,percentTats,iceCream])
+	classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3)
+	print("You will probably like this person: ", resultList[classifierResult - 1])
+###############################
+# datingDataMat, datingLabels = file2matrix("datingTestSet.txt");
+# print(datingDataMat)
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*array(datingLabels), 15.0*array(datingLabels))
+# fig.show()
+# normMat, ranges, minVals = autoNorm(datingDataMat)
+# x = input("x: ")
